73bf4f71d38344c50b5165c10eec6f15f122c7b8
Gaurav <80704349+Gaurav0818@users.noreply.github.com>
2024-11-22 17:38:20 +0530

    Initial Layout Changse +TMP

---
+++ CalculatorApp/Assets/Script/SetPreferredRatio.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SetPreferredRatio : MonoBehaviour
+{
+    [SerializeField] int _ratio;
+
+    public int GetRatio() => _ratio;
+
+}
---
+++ CalculatorApp/Assets/Script/SortBasedOnPreferredRatio.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SortBasedOnPreferredRatio : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _lg;
+    [SerializeField] private int _ratioCount = 0;
+    [SerializeField] private int _padding = 0; 
+    void Start()
+    {
+        float pHight = transform.GetComponent<RectTransform>().rect.height - 2*_padding;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            var ratioComp = child.GetComponent<SetPreferredRatio>();
+            if (ratioComp)
+            {
+                _lg.Add(child);
+                _ratioCount += ratioComp.GetRatio();
+            }
+        }
+
+        foreach (var child in _lg)
+        {
+            int ratio = child.GetComponent<SetPreferredRatio>().GetRatio();
+            RectTransform rt = child.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, ratio * pHight / _ratioCount);
+        }
+    }
+}
